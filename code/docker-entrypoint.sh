7dd341f93c4b329573650862637e5595935f7dc0
Adrian <adinsoonS@gmail.com>
2021-03-06 23:05:04 +0100

    Init commit, postgres connection, docker setup

---
+++ code/docker-entrypoint.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#credits: KNR Web
+
+ownership() {
+    # Fixes ownership of output files
+    # source: https://github.com/BD2KGenomics/cgl-docker-lib/blob/master/mutect/runtime/wrapper.sh#L5
+    user_id=$(stat -c '%u:%g' /code)
+    chown -R "${user_id}" /code
+}
+
+echo "Installing missing packets"
+pip install -r ./requirements.txt
+echo "Waiting for postgres"
+chmod +x wait-for-it.sh
+./wait-for-it.sh -t 80 "$POSTGRES_SERVICE":5432 || exit 1
+
+echo '--------------------------'
+echo 'Database migration'
+echo '--------------------------'
+python manage.py makemigrations || exit 1
+python manage.py migrate || exit 1
+
+
+echo '--------------------------'
+echo 'Run test'
+echo '--------------------------'
+python manage.py test --noinput
+
+
+echo '--------------------------'
+echo 'Fixing ownership of files'
+echo '--------------------------'
+ownership
+
+echo '--------------------------'
+echo 'Run command'
+# shellcheck disable=SC2068
+echo $@
+echo '--------------------------'
+# shellcheck disable=SC2068
+python manage.py $@ || exit 1
