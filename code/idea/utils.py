451362b4e65d3dbaa0905d683bd57205377ae58f
Adrian <adinsoonS@gmail.com>
2021-03-12 20:11:48 +0100

    update slugify function
    Took 23 minutes

--- code/idea/utils.py
+++ code/idea/utils.py
@@ -5,5 +5,5 @@ from django.utils.text import slugify
 def custom_slugify(content):
     content = slugify(content)
     addon = get_random_string(5)
-    content = content + '-' + addon
+    content = content + '---' + addon
     return content
